2944a6495c766ab3df5a3e5ed6b3ec05028f0f49
CKiilu <cndeti@gmail.com>
2016-04-06 18:34:42 +0300

    Added group, connect and disconnect to functionality

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,7 @@
-from channels import Group
-
 # from django.http import HttpResponse
 # from channels.handler import AsgiHandler
+
+from channels import Group
 
 # Create your views here.
 # def http_consumer(message):
@@ -11,10 +11,18 @@ from channels import Group
 # 	for chunk in AsgiHandler.encode_response(response):
 # 		message.reply_channel.send(chunk)
 
+# Connected to websocket.connect - connects service
+def ws_add(message):
+	Group("chat").add(message.reply_channel)
 
+# Connected to websocket.receive - allows message receipt
 def ws_message(message):
 	# ASGI WebSocket packet received and send packet message types
 	# Both have text key for textual data
-	message.reply_channel.send({
-			"text": message.content['text']
+	Group("chat").send({
+			"text": "[user] %s" % message.content['text']
 		})
+
+# Connected to websocket.diconnect - disconnects service
+def ws_disconnect(message):
+	Group("chat").discard(message.reply_channel)
